84e825e120530c00efad7e715b2b25696e4dcecb
Gabriel Peressini <gabe.peressini@gmail.com>
2021-09-29 23:51:13 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gany
+
+Developed with Unreal Engine 4
---
+++ Source/gany/ganyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ganyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GANY_API AganyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
